38c1338737b3e35b920efdc5ffecd7b58f17e8ea
martia_f <44898501+MinecraftPublisher@users.noreply.github.com>
2022-08-22 22:03:37 +0430

    Added support for specific entries

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,17 @@ import * as vscode from 'vscode';
 
 import { search } from './utils/search';
 import { matchSearchPhrase } from './utils/matchSearchPhrase';
+import { getConfig } from './config';
+import Parsers from './utils/parsers/index';
+
+let runningParsers = false;
+let cancelEvent = false;
 
 export function activate(_: vscode.ExtensionContext) {
 
     const provider: vscode.CompletionItemProvider = {
         // @ts-ignore
-        provideInlineCompletionItems: async (document, position, context, token) => {
+        provideInlineCompletionItems: async (document : vscode.TextDocument, position, context, token) => {
 
             const textBeforeCursor = document.getText(
                 new vscode.Range(position.with(undefined, 0), position)
@@ -33,7 +38,56 @@ export function activate(_: vscode.ExtensionContext) {
                 } catch (err: any) {
                     vscode.window.showErrorMessage(err.toString());
                 }
+            } else {
+                /* check if parsers are enabled */
+                if(getConfig().settings.enableParsers) {
+                    if(runningParsers) cancelEvent = true;
+
+                    runningParsers = true;
+                    /* result has to be determined using parsers. */
+                    const parserText = document.getText(
+                        new vscode.Range(position.with(0, 0), position)
+                    );
+
+                    /* find enabled parsers and ask them for a result! */
+                    Parsers.filter((parser) => parser.isEnabled())
+                    .forEach(async (parser) => {
+
+                        parser.provideInlineCodeCompletions(
+                            parserText,
+                            document.fileName,
+                            getConfig().settings.huggingfaceToken
+                        ).then((result) => {
+                            items.push({
+                                text: result,
+                                insertText: result,
+                                range: new vscode.Range(position.translate(0, result.length), position)
+                            });
+                        })
+                        
+                        return await new Promise((resolve, reject) => {
+                            setInterval(() => {
+                                if(cancelEvent) resolve();
+                            }, 100);
+                        });
+                    });
+
+                    cancelEvent = false;
+                    await new Promise((resolve, reject) => {
+                        let iterations = 0;
+                        setInterval(() => {
+                            if(cancelEvent) reject(null);
+
+                            iterations++;
+                            if(items.length === Parsers.length) resolve(null);
+                            else if (iterations > (5 * 10)) resolve(null);
+                        }, 100);
+                    });
+
+                    runningParsers = false;
+                }
             }
+
             return {items};
         },
     };
